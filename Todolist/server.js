b0d02f3f781c30245e191ac12cc8721ca6de7f6d
Chris <hakizachrist1@gmail.com>
2023-01-25 14:31:38 +0200

    Fix linters

--- Todolist/server.js
+++ Todolist/server.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unresolved */
 const express = require('express');
 const webpack = require('webpack');
+
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const app = express();
+
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
