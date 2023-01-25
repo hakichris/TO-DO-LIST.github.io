9b3737fad391b62c7e08028c9cbca9faf0b0a082
Chris <hakizachrist1@gmail.com>
2023-01-25 15:00:43 +0200

    Fix linters

--- Todolist/server.js
+++ Todolist/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const webpack = require('webpack');
 
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const app = express();
 
 const config = require('./webpack.config.js');
+
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
