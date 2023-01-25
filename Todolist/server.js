2d5d822d21956762e9fda16149553efeb840de2a
Chris <hakizachrist1@gmail.com>
2023-01-25 14:26:05 +0200

    Fix linters

--- Todolist/server.js
+++ Todolist/server.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-unresolved */
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-
 const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
@@ -15,6 +15,6 @@ app.use(
 );
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
+app.listen(3000, () => {
+  // console.log('Example app listening on port 3000!\n');
 });
